854666ba8b25ea4ba8a86f2ee2481e4db20abd7d
1758828 <73910792+1758828@users.noreply.github.com>
2021-08-31 13:43:04 -0500

    creación de repositorio del taller 4

---
+++ src/main/java/com/tcs/tallerListas/beans/Estudiante.java
@@ -0,0 +1,48 @@
+package com.tcs.tallerListas.beans;
+
+public class Estudiante {
+	private String id;
+    private String nombre;
+    private Integer edad;
+    private String grado;
+    private String aprobo;
+    
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public String getGrado() {
+		return grado;
+	}
+	public void setGrado(String grado) {
+		this.grado = grado;
+	}
+	public String getAprobo() {
+		return aprobo;
+	}
+	public void setAprobo(String aprobo) {
+		this.aprobo = aprobo;
+	}
+    
+	
+    
+    
+	
+
+}
